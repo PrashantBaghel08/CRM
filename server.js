adacf7ea04f9ad761835f8a288ab24472ea2251d
Prashant Baghel <42498668+PrashantBaghel08@users.noreply.github.com>
2025-06-06 16:00:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 //const db = require('./config/db');
 const userRoutes = require('./routes/userRoutes');
+const warehouseRoutes = require('./routes/warehouseRoutes');
 
 
 
@@ -23,7 +24,7 @@ app.use(express.json());
 
 // Routes
 app.use('/api/users', userRoutes);
-
+app.use('/api/warehouses', warehouseRoutes);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
